9c7f735b73b07319b2535c7cbe29f7f275568486
AnggaWdnt <angga.widianto.ft22@mail.umy.ac.id>
2024-11-22 17:47:57 +0700

    tambahan fungsi RencanaStudyView

---
+++ app/src/main/java/com/example/praktikum8/ui/view/screen/RencanaStudyView.kt
@@ -0,0 +1,14 @@
+package com.example.praktikum8.ui.view.screen
+
+import androidx.compose.runtime.Composable
+import com.example.praktikum8.ui.model.Mahasiswa
+import com.example.praktikum8.ui.model.RencanaStudi
+
+@Composable
+fun RencanaStudyView(
+    mahasiswa: Mahasiswa,
+    onSubmitButtonClicked: (MutableList<String>) -> Unit,
+    onBackbuttonClicked: () -> Unit
+){
+
+}
