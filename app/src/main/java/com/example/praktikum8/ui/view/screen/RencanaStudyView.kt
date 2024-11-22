b15b87da9b2b0d2b37cdc92b3a94f4d64997d644
AnggaWdnt <angga.widianto.ft22@mail.umy.ac.id>
2024-11-22 17:51:17 +0700

    tambahan variable choseDropdown pada RencanaStudyView

--- app/src/main/java/com/example/praktikum8/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/praktikum8/ui/view/screen/RencanaStudyView.kt
@@ -1,6 +1,10 @@
 package com.example.praktikum8.ui.view.screen
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import com.example.praktikum8.ui.model.Mahasiswa
 import com.example.praktikum8.ui.model.RencanaStudi
 
@@ -10,5 +14,9 @@ fun RencanaStudyView(
     onSubmitButtonClicked: (MutableList<String>) -> Unit,
     onBackbuttonClicked: () -> Unit
 ){
-
+    var choseDropdown by remember {
+        mutableStateOf(
+            ""
+        )
+    }
 }
